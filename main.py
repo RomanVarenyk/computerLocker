4e5af60bb89cc51eda42ac1f354c492185569d98
Punchline <roma.bystriakov@gmail.com>
2022-05-18 14:12:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import ctypes
+from tkinter import ttk
+from tkinter.messagebox import askyesno
+
+root =Tk()
+
+# click event handler
+def confirm():
+    answer = askyesno(title='confirmation',
+                    message='ya sure bud?')
+    if answer:
+        root.destroy()
+        ctypes.windll.user32.LockWorkStation()
+    else:
+        root.destroy()
+
+confirm()
+# start the app
+root.mainloop()
+
