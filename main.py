a9fbe89f4a0eb7d216c6e23431426f3ed69497c0
Punchline <roma.bystriakov@gmail.com>
2022-05-19 18:05:24 -0400

    Instead of lockdown, just new webpage

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import random
+import webbrowser
 from tkinter import *
-import ctypes
-from tkinter import ttk
 from tkinter.messagebox import askyesno
+
+import pyautogui
 
 root = Tk()
 
 
-# click event handler
+def doDesktop():
+    websites = ["dcps.instructure.com", "https://dcps.instructure.com/courses/237226", "https://dcps.instructure.com/courses/237724", "https://dcps.instructure.com/courses/236450",
+            "https://dcps.instructure.com/courses/237343", "https://dcps.instructure.com/courses/235737", "https://dcps.instructure.com/courses/237343/modules", "https://dcps.instructure.com/courses/237226/modules"]
+    pyautogui.hotkey('winleft', 'd')
+    brave_file = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe %s"
+    webbrowser.get(brave_file).open_new_tab(random.choice(websites))
+
+
 def confirm():
     answer = askyesno(title='confirmation',
                       message='ya sure bud?')
     if answer:
         root.destroy()
-        ctypes.windll.user32.LockWorkStation()
+        doDesktop()
     else:
         root.destroy()
 
 
-confirm()
+# confirm()
 # start the app
-root.mainloop()
+# root.mainloop()
+doDesktop()
