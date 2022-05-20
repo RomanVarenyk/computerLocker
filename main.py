c53dcd41fd15bc579dc55f897d14b137c052728e
Punchline <roma.bystriakov@gmail.com>
2022-05-20 17:29:43 -0400

    Final build

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from tkinter.messagebox import askyesno
 import pyautogui
 
 root = Tk()
-
+websites = []
+def getWebsites():
+    webList =  open("websites.txt", "r")
+    for i in webList.readlines():
+        websites.append(i)
 
 def doDesktop():
-    websites = ["dcps.instructure.com", "https://dcps.instructure.com/courses/237226",
-                "https://dcps.instructure.com/courses/237724", "https://dcps.instructure.com/courses/236450",
-                "https://dcps.instructure.com/courses/237343", "https://dcps.instructure.com/courses/235737",
-                "https://dcps.instructure.com/courses/237343/modules",
-                "https://dcps.instructure.com/courses/237226/modules"]
+
     pyautogui.hotkey('winleft', 'd')
     brave_file = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe %s"
     webbrowser.get(brave_file).open(random.choice(websites))
@@ -33,4 +33,5 @@ def confirm():
 # start the app
 # root.mainloop()
 # auto-py-to-exe
+getWebsites()
 doDesktop()
